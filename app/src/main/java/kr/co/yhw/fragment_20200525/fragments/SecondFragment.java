7f01baaae3c9e635f98378c7662339cac63897b5
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-25 17:09:59 +0900

    두번째 프레그먼트 연습문제  - getActivity() 참조

--- app/src/main/java/kr/co/yhw/fragment_20200525/fragments/SecondFragment.java
+++ app/src/main/java/kr/co/yhw/fragment_20200525/fragments/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,34 +12,32 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import kr.co.yhw.fragment_20200525.R;
-import kr.co.yhw.fragment_20200525.databinding.FragmentFirstBinding;
+import kr.co.yhw.fragment_20200525.databinding.FragmentSecondBinding;
 
-public class FirstFragment extends Fragment {
+public class SecondFragment extends Fragment {
 
-    FragmentFirstBinding binding;
+    FragmentSecondBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding =  DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second,container,false);
         return binding.getRoot();
-
     }
-//    나를 담고있는 액티비티의 onCreate가 실행된 시점
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-//        액티비티의 onCreate의 여러가지 기능 작성
-
-        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+        
+        
+        binding.secondButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String input = binding.messageEdt.getText().toString();
-                binding.myTxt.setText(input);
+                Toast.makeText(getActivity(), "두번째 프래그먼트 토스트", Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
+    
+
 }
+
